79a28d5fa3fb219faead040fd8a9006f9fe91801
Martijn9601 <43539448+Martijn9601@users.noreply.github.com>
2018-11-21 11:37:17 +0100

    edited MeetingController

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Meeting;
 
 class MeetingController extends Controller
 {
     public function __construct()
     {
-        //this->middleware('name');
-    }   
+        // $this->middleware('name');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -17,59 +23,160 @@ class MeetingController extends Controller
      */
     public function index()
     {
-        return "It works!";
+        $meetings = Meeting::all();
+        foreach ($meetings as $meeting) {
+            $meeting->view_meeting = [
+                'href' => 'api/v1/meeting/' . $meeting->id,
+                'method' => 'GET'
+            ];
+        }
+
+        $response = [
+            'msg' => 'List of all Meetings',
+            'meetings' => $meetings
+        ];
+        return response()->json($response, 200);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Store a newly created resource in storage.
      *
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required',
+            'time' => 'required|date_format:YmdHie',
+            'user_id' => 'required'
+        ]);
+
         $title = $request->input('title');
         $description = $request->input('description');
         $time = $request->input('time');
         $user_id = $request->input('user_id');
-        return "It works!";
+
+        $meeting = new Meeting([
+            'time' => Carbon::createFromFormat('YmdHie', $time),
+            'title' => $title,
+            'description' => $description
+        ]);
+        if ($meeting->save()) {
+            $meeting->users()->attach($user_id);
+            $meeting->view_meeting = [
+                'href' => 'api/v1/meeting/' . $meeting->id,
+                'method' => 'GET'
+            ];
+            $message = [
+                'msg' => 'Meeting created',
+                'meeting' => $meeting
+            ];
+            return response()->json($message, 201);
+        }
+
+        $response = [
+            'msg' => 'Error during creationg'
+        ];
+
+        return response()->json($response, 404);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        return "It works!";
+        $meeting = Meeting::with('users')->where('id', $id)->firstOrFail();
+        $meeting->view_meetings = [
+            'href' => 'api/v1/meeting',
+            'method' => 'GET'
+        ];
+
+        $response = [
+            'msg' => 'Meeting information',
+            'meeting' => $meeting
+        ];
+        return response()->json($response, 200);
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Update the specified resource in storage.
      *
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required',
+            'time' => 'required|date_format:YmdHie',
+            'user_id' => 'required'
+        ]);
+
         $title = $request->input('title');
         $description = $request->input('description');
         $time = $request->input('time');
         $user_id = $request->input('user_id');
-        return "It works!";
+
+        $meeting = Meeting::with('users')->findOrFail($id);
+
+        if (!$meeting->users()->where('users.id', $user_id)->first()) {
+            return response()->json(['msg' => 'user not registered for meeting, update not successful'], 401);
+        };
+        $meeting->time = Carbon::createFromFormat('YmdHie', $time);
+        $meeting->title = $title;
+        $meeting->description = $description;
+        if (!$meeting->update()) {
+            return response()->json(['msg' => 'Error during updating'], 404);
+        }
+
+        $meeting->view_meeting = [
+            'href' => 'api/v1/meeting/' . $meeting->id,
+            'method' => 'GET'
+        ];
+
+        $response = [
+            'msg' => 'Meeting updated',
+            'meeting' => $meeting
+        ];
+
+        return response()->json($response, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        return "It works!";
+        $meeting = Meeting::findOrFail($id);
+        $users = $meeting->users;
+        $meeting->users()->detach();
+        if (!$meeting->delete()) {
+            foreach ($users as $user) {
+                $meeting->users()->attach($user);
+            }
+            return response()->json(['msg' => 'deletion failed'], 404);
+        }
+
+        $response = [
+            'msg' => 'Meeting deleted',
+            'create' => [
+                'href' => 'api/v1/meeting',
+                'method' => 'POST',
+                'params' => 'title, description, time'
+            ]
+        ];
+
+        return response()->json($response, 200);
     }
 }
