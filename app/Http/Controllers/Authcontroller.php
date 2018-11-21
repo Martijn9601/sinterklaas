262efff0b73954ca74560a02a015fec6122927d3
Martijn9601 <43539448+Martijn9601@users.noreply.github.com>
2018-11-21 11:21:38 +0100

    edited Authcontroller

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -2,22 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
-class Authcontroller extends Controller
+use App\Http\Requests;
+
+class AuthController extends Controller
 {
     public function store(Request $request)
     {
-    	$name = $request->input('name');
-    	$email = $request->input('email');
-    	$password = $request->input('password');
-    	return "It works!";
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:5'
+        ]);
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = new User([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password)
+        ]);
+
+        if ($user->save()) {
+            $user->signin = [
+                'href' => 'api/v1/user/signin',
+                'method' => 'POST',
+                'params' => 'email, password'
+            ];
+            $response = [
+                'msg' => 'User created',
+                'user' => $user
+            ];
+            return response()->json($response, 201);
+        }
+
+        $response = [
+            'msg' => 'An error occurred'
+        ];
+
+        return response()->json($response, 404);
     }
 
     public function signin(Request $request)
     {
-    	$email = $request->input('email');
-    	$password = $request->input('password');
-    	return "It works!";
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $user = [
+            'name' => 'Name',
+            'email' => $email,
+            'password' => $password
+        ];
+
+        $response = [
+            'msg' => 'User signed in',
+            'user' => $user
+        ];
+
+        return response()->json($response, 200);
     }
 }
--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -9,7 +9,7 @@ class RegistrationController extends Controller
     public function store(Request $request)
     {
         $meeting_id = $request->input('meeting_id');
-        $user_id = $request->input('user_id')
+        $user_id = $request->input('user_id');
         return "It works!";
     }
 
